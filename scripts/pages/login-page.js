86d1d5f6e62b50a9c8becd477383c91db8c572b9
emachipana <emachipana0102@gmail.com>
2022-05-27 18:55:27 -0500

    ADD: partial solution

--- scripts/pages/login-page.js
+++ scripts/pages/login-page.js
@@ -1,86 +1,23 @@
-// REVISAR COMENTARIOS
-import { login } from "../services/sessions-service.js";
-import DOMHandler from "../dom-handler.js";
-import { input  } from "../components/input.js";
-import STORE from "../store.js";
-import HomePage from "./homepage.js";
+import Login from "../components/login.js" 
+import currentPage from "../currentPageSession.js";
 
 function render() {
-
-  const { loginError } = LoginPage.state;
+  const page = currentPage.page;
   return `
     <main class="session-container">
-        <h1 class="session__title">Login</h1>
-        <hr class="hr">
-        <form class="session__form">
-          <div class="form__input-container">
-            ${input({
-              type: "email",
-              placeholder: "example@gmail.com",
-              required: true,
-              value: "team01@mail.com" // quitar el value al final
-            })}
-
-            ${input({
-              type: "password",
-              placeholder: "******",
-              required: true,
-              value: "123456" // quitar el value al final
-            })}
-
-            ${loginError ? 
-              `<p class="text-center error-300">${loginError}</p>`: ''
-            }
-          </div>
-          <div class="session__buttons">
-            <hr class="hr">
-            <a href="#" class="button__link">Signup</a>
-            <button class="button__link" type="submit">Login</button>
-          </div>
-        </form>
+      ${page === "login" ? Login : ""}
+      ${page === "signup" ? SignUp : ""}
     </main>
-  `;
-}
-
-function listenSubmitForm() {
-  const form =  document.querySelector(".session__form")
-
-  form.addEventListener("submit", async (event) => {
-    try {
-      event.preventDefault();
-  
-      const { email, password } = event.target;
-  
-      const credentials = {
-        email: email.value,
-        password: password.value,
-      }
-  
-      const user = await login(credentials)
-      STORE.user = user
-      // console.log(STORE)
-
-      await STORE.filterContacts()
-      DOMHandler.load(HomePage)
-    } catch (error) {
-      // this.state.loginError = error.message
-      LoginPage.state.loginError = error.message
-      DOMHandler.reload()
-    }
-  })
+  `
 }
 
 const LoginPage = {
   toString() {
-    // return render.call(this)
-    return render()
+    return render();
   },
   addListeners() {
-    // listenSubmitForm.call(this)
-    return listenSubmitForm()
-  },
-  state: {
-    loginError: null,
+    if(currentPage==="login") Login.addListeners();
+    if(currentPage==="signup") SignUp.addListeners();
   }
 }
 
